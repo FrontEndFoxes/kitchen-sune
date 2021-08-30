3ea7ca729a784a553b7faa8d12d93c9f60fe132d
Jen Looper <jen.looper@gmail.com>
2021-08-30 16:25:25 -0400

    config edits

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,67 +1,46 @@
 const { description } = require('../../package')
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Front-End Foxes Community Recipe Book',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
+  base: "/kitchen-sune/",
+  title: "Front-End Foxes Community Recipe Book",
   description: description,
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    repo: "",
+    editLinks: true,
+    lastUpdated: true,
     nav: [
       {
-        text: 'Recipes',
-        link: '/recipes/',
+        text: "Recipes",
+        link: "/recipes/",
       },
       {
-        text: 'Front-End Foxes',
-        link: 'https://frontendfoxes.school'
+        text: "Front-End Foxes",
+        link: "https://frontendfoxes.school",
       },
-      
     ],
     sidebar: {
-      '/recipes/': [
+      "/recipes/": [
         {
-          title: 'Recipes',
+          title: "Recipes",
           collapsable: false,
-          children: [
-            '',
-            'roasted-brussels-sprouts',
-          ]
-        }
+          children: ["", "roasted-brussels-sprouts"],
+        },
       ],
-    }
+    },
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+};
