a3e74fdf998fd9a348c011413ce655297c08878c
cynthia <glean@cynthiateeters.com>
2021-09-04 15:37:57 -0400

    Add cranberry relish recipe

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,43 +1,40 @@
-const { description } = require("../../package");
+const { description } = require('../../package');
 
 module.exports = {
-  base: "/kitchen-sune/",
-  title: "Front-End Foxes Community Recipe Book",
+  base: '/kitchen-sune/',
+  title: 'Front-End Foxes Community Recipe Book',
   description: description,
 
   head: [
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
-    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
-    [
-      "meta",
-      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    ],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ],
 
   themeConfig: {
-    repo: "",
+    repo: '',
     editLinks: true,
     lastUpdated: true,
     nav: [
       {
-        text: "Recipes",
-        link: "/recipes/",
+        text: 'Recipes',
+        link: '/recipes/',
       },
       {
-        text: "Front-End Foxes",
-        link: "https://frontendfoxes.school",
+        text: 'Front-End Foxes',
+        link: 'https://frontendfoxes.school',
       },
       {
-        text: "GitHub",
-        link: "https://github.com/frontendfoxes/kitchen-sune",
+        text: 'GitHub',
+        link: 'https://github.com/frontendfoxes/kitchen-sune',
       },
     ],
     sidebar: {
-      "/recipes/": [
+      '/recipes/': [
         {
-          title: "Recipes",
+          title: 'Recipes',
           collapsable: false,
-          children: ["", "roasted-brussels-sprouts", "nigerian-jollof-rice", "cereal-with-protein-powder", "ukrainian-varenyky"],
+          children: ['', 'roasted-brussels-sprouts', 'nigerian-jollof-rice', 'cereal-with-protein-powder', 'ukrainian-varenyky', 'cranberry-relish'],
         },
       ],
     },
@@ -46,5 +43,5 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
 };
---
+++ src/recipes/cranberry-relish.md
@@ -0,0 +1,32 @@
+# Maple Walnut Cranberry Relish
+
+I make this relish every fall for our Thanksgiving dinner, celebrated in the US on the 3rd Thursday in November. It's a little tart and, according to my family, so much better than the canned sauce.
+
+Since the relish lasts for several weeks, we use often the extras with breakfast toast. It can also be used as a topping on vanilla ice cream.
+
+Thanks to [Chef John at AllRecipes.com](https://www.allrecipes.com/recipe/235618/maple-walnut-cranberry-sauce/) for the inspiration. 😍
+
+## Ingredients
+
+- 1 (12 ounce) package fresh cranberries, rinsed and drained
+- 3/4 cup orange juice (with pulp)
+- 1/2 cup maple syrup
+- 1/4 cup brown sugar
+- 1 tablespoon grated ginger
+- 1 tablespoon grated orange zest
+- 1 cinnamon stick
+- 3/4 cup chopped walnuts
+
+## Method
+
+1. Place cranberries into a large saucepan and stir in orange juice, maple syrup, sugar, ginger, orange zest, and cinnamon stick.
+2. Bring sauce to a simmer. Berries will begin to pop. Cook at a simmer until berries are cooked through and soft, about 10 minutes.
+3. Remove saucepan from heat and let cranberry sauce cool for about 5 minutes.
+4. Stir the walnuts in a dry skillet over medium heat, cooking until golden brown and fragrant, about 5 minutes.
+5. Vigorously shake the walnuts in a colander to remove the bitter outer skin.
+6. Stir walnuts into cranberry sauce. Let cool to room temperature and transfer to a serving bowl. The relish will thicken as it cools.
+7. Refrigerate until needed. For best results, give the relish a few days before serving.
+
+## Author
+
+Cheers, Cynthia Teeters
