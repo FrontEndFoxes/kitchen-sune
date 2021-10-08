61da590cc346196124fe1b30ea55ea5db383642f
Crislana Rafael <crislanarafael@gmail.com>
2021-10-08 17:05:09 -0600

    Added 'strawberry-mush.md' recipe to kitchen-sune

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,40 +1,57 @@
-const { description } = require('../../package');
+const { description } = require("../../package");
 
 module.exports = {
-  base: '/kitchen-sune/',
-  title: 'Front-End Foxes Community Recipe Book',
+  base: "/kitchen-sune/",
+  title: "Front-End Foxes Community Recipe Book",
   description: description,
 
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   themeConfig: {
-    repo: '',
+    repo: "",
     editLinks: true,
     lastUpdated: true,
     nav: [
       {
-        text: 'Recipes',
-        link: '/recipes/',
+        text: "Recipes",
+        link: "/recipes/",
       },
       {
-        text: 'Front-End Foxes',
-        link: 'https://frontendfoxes.school',
+        text: "Front-End Foxes",
+        link: "https://frontendfoxes.school",
       },
       {
-        text: 'GitHub',
-        link: 'https://github.com/frontendfoxes/kitchen-sune',
+        text: "GitHub",
+        link: "https://github.com/frontendfoxes/kitchen-sune",
       },
     ],
     sidebar: {
-      '/recipes/': [
+      "/recipes/": [
         {
-          title: 'Recipes',
+          title: "Recipes",
           collapsable: false,
-          children: ['','nigerian-puff-puff', 'roasted-brussels-sprouts', 'argentinian-chimichurri', 'nigerian-jollof-rice', 'cereal-with-protein-powder', 'ukrainian-varenyky', 'cranberry-relish', 'pandemic-cookies', 'orange-braised-pork-chops', 'sweet-potato-lentil-curry', 'nigerian-unripe-plantain-porridge'],
+          children: [
+            "",
+            "nigerian-puff-puff",
+            "roasted-brussels-sprouts",
+            "argentinian-chimichurri",
+            "nigerian-jollof-rice",
+            "cereal-with-protein-powder",
+            "ukrainian-varenyky",
+            "cranberry-relish",
+            "pandemic-cookies",
+            "orange-braised-pork-chops",
+            "sweet-potato-lentil-curry",
+            "nigerian-unripe-plantain-porridge",
+            "strawberry-mush",
+          ],
         },
       ],
     },
@@ -43,5 +60,5 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
 };
